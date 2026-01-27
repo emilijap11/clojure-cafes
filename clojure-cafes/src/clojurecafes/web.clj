58f0d21b4cbc12d5b2670cef35bed0d68b4c9c9b
emilijap11 <emilijaprvulovic3@gmail.com>
2026-01-27 17:33:28 +0100

    Update web.clj

--- clojure-cafes/src/clojurecafes/web.clj
+++ clojure-cafes/src/clojurecafes/web.clj
@@ -6,24 +6,24 @@
         [:head
          [:title "Preporučeni kafici"]
          ; [:link {:rel "stylesheet" :href "/style.css"}]
-         [:link {:rel "stylesheet" :href (str "/style.css?v=" (System/currentTimeMillis))}]]))]
-        [:body
-         [:h1 "Preporučeni kafici"]
-         (if (empty? cafes)
-           [:p "Nema dostupnih kafica za vaše kriterijume."]
-           [:ul
-            (for [c cafes]
-                 [:li
-                  [:strong (:name c)] " – "
-                  (:location c)
-                  [:br]
-                  "Otvoreno do: " (:open-until c) "h"
-                  [:br]
-                  ;"Udaljenost: " (format "%.2f" (:distance c)) " km"
-                  [:br]
-                  (:note c)
-                  [:br]
-                  [:a {:href (str "https://www.google.com/maps/search/?api=1&query="
-                                  (:lat c) "," (:lng c))
-                       :target "_blank"}
-                   "Pogledaj na mapi"]])])]))
+         [:link {:rel "stylesheet" :href (str "/style.css?v=" (System/currentTimeMillis))}]]))
+[:body
+ [:h1 "Preporučeni kafici"]
+ (if (empty? cafes)
+   [:p "Nema dostupnih kafica za vaše kriterijume."]
+   [:ul
+    (for [c cafes]
+         [:li
+          [:strong (:name c)] " – "
+          (:location c)
+          [:br]
+          "Otvoreno do: " (:open-until c) "h"
+          [:br]
+          ;"Udaljenost: " (format "%.2f" (:distance c)) " km"
+          [:br]
+          (:note c)
+          [:br]
+          [:a {:href (str "https://www.google.com/maps/search/?api=1&query="
+                          (:lat c) "," (:lng c))
+               :target "_blank"}
+           "Pogledaj na mapi"]])])]
